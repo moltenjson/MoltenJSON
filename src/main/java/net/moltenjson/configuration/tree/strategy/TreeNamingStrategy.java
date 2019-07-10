56075efa522ef537fc4d9322145842f9070b8ea1
ReflxctionDev <aloosh.kasasbeh@gmail.com>
2019-07-10 15:53:08 +0300

    Update license appendix

--- src/main/java/net/moltenjson/configuration/tree/strategy/TreeNamingStrategy.java
+++ src/main/java/net/moltenjson/configuration/tree/strategy/TreeNamingStrategy.java
@@ -1,5 +1,5 @@
 /*
- * * Copyright 2019 github.com/ReflxctionDev
+ * * Copyright 2019 github.com/moltenjson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
